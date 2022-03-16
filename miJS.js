f4fc16250c4623979829db0ee239030ffa6b5539
juanlopez97 <70778455+juanlopez97@users.noreply.github.com>
2022-03-15 20:02:48 -0500

    js

---
+++ miJS.js
@@ -0,0 +1,46 @@
+const rol ="profesor";
+let role;
+console.log(rol);
+console.log(role);
+role= "student";
+console.log(role);
+console.log("Hola, mi rol es "+role+" de la UAO");
+console.log(`Hola, mi rol es ${role} de la UAO`);
+let a = 11;
+let b = 5;
+let modulo = a%b;
+console.log(modulo);
+
+let c = "1";
+let d = 1;
+if(c === 1){
+    console.log("son iguales");
+}else{
+    console.log("no son iguales");
+}
+
+c === 1 ? console.log("son iguales") : console.log("no son iguales");
+
+let e = "5";
+let f = 5;
+if( e === "5" && f===5){
+
+    console.log("son iguales");
+}else{
+    console.log("no son iguales");
+}
+
+if( e === "5" || f=== 6){
+
+    console.log("son iguales");
+}else{
+    console.log("no son iguales");
+}
+
+if( e != f){
+
+    console.log("son iguales");
+}else{
+    console.log("no son iguales");
+}
+
